02ff06acdc8df05d43c26ba771ebd34ad908aad8
Alex Wiltschko <alex.bw@gmail.com>
2014-07-22 11:00:58 -0400

    tmux alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -170,3 +170,5 @@ alias sf='fasd -sif'     # interactive file selection
 alias z='fasd_cd -d'     # cd, same functionality as j in autojump
 alias zz='fasd_cd -d -i' # cd with interactive selection
 alias pl='ipython --pylab'
+alias sc='starcluster'
+alias tad='tmux attach -d'
